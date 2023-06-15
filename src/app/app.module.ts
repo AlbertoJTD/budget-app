ddfcb0ea888906f7fe93f4d758ef9bcaabbdf85f
AlbertoJTD <albertojtd@outlook.com>
2023-06-15 15:14:09 -0600

    Add from componen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SummaryComponent } from './summary/summary.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryComponent
+    SummaryComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
