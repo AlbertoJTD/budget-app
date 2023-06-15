5c358033f46619e9308227d45bbe45d1e01bedb7
AlbertoJTD <albertojtd@outlook.com>
2023-06-15 15:16:45 -0600

    Add revenues component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SummaryComponent } from './summary/summary.component';
 import { FormComponent } from './form/form.component';
+import { RevenuesComponent } from './revenues/revenues.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
-    FormComponent
+    FormComponent,
+    RevenuesComponent
   ],
   imports: [
     BrowserModule
