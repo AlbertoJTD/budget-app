403d2a3b99bff7cdb68f064e6d385d9a9a8a9eae
AlbertoJTD <albertojtd@outlook.com>
2023-06-16 09:51:21 -0600

    Add get functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Revenue } from './revenues/revenues.model';
+import { Expense } from './expenses/expenses.model';
+import { RevenuesService } from './revenues/revenue.service';
+import { ExpensesService } from './expenses/expense.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'budget-app';
+
+  revenues: Revenue[] = [];
+  expenses: Expense[] = [];
+
+  constructor(private revenueService: RevenuesService, private expenseService: ExpensesService) {
+    this.revenues = revenueService.revenues;
+    this.expenses = expenseService.expenses;
+  }
+
+  getTotalRevenue(): number {
+    let total: number = 0;
+    this.revenues.forEach(revenue => {
+      total += revenue.amount;
+    });
+
+    return total;
+  }
+
+  getTotalExpense(): number {
+    let total: number = 0;
+    this.expenses.forEach(expense => {
+      total += expense.amount;
+    });
+
+    return total;
+  }
+
+  getTotalPercentage(): number {
+    return  this.getTotalExpense() / this.getTotalRevenue();
+  }
+
+  getTotalBudget(): number {
+    return this.getTotalRevenue() - this.getTotalExpense();
+  }
 }
